75749def711fc27a5c9304a2a047e43731a446e9
Zheng Hao Tan <hi@zhenghaotan.com>
2022-11-17 22:49:37 -0800

    add CountUp component

---
+++ is-sbf-in-prison-yet/src/components/CountUp.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+export const useCountup = (startDate: string | number | Date) => {
+  const countUpDate = new Date(startDate).getTime();
+
+  const [countUp, setCountUp] = useState(
+    new Date().getTime() - countUpDate,
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountUp(new Date().getTime() - countUpDate);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [countUpDate]);
+
+  return getReturnValues(countUp);
+};
+
+const getReturnValues = (countUp: number) => {
+  // calculate time left
+  const days = Math.floor(countUp / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (countUp % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+  const minutes = Math.floor((countUp % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((countUp % (1000 * 60)) / 1000);
+
+  return [days, hours, minutes, seconds];
+};
